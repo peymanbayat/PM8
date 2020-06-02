5360e2239d97e6af1b1a2df4c206dec161e84120
SAKTHI ANAND <33320538+sakthianand7@users.noreply.github.com>
2020-06-02 21:25:02 +0530

    Update MLApplication.py

--- MLApplication.py
+++ MLApplication.py
@@ -57,7 +57,7 @@ def main():
 				plt.ylim(X2.min(), X2.max())
 				for i, j in enumerate(np.unique(y_set)):
 				    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-				                c = ListedColormap(('red', 'green'))(i), label = j)
+				                c = ListedColormap(('darkred', 'green'))(i), label = j)
 				plt.xlabel('Age')
 				plt.ylabel('Estimated Salary')
 				plt.legend()
